b3ddb239ea8ed92da84a848b9381ba92577adee1
dknoma <djknoma@gmail.com>
2020-01-28 13:08:57 -0800

    added mongo-java-driver

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -3,11 +3,17 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.mongodb.DB;
+import com.mongodb.client.MongoDatabase;
 
 public class Tester {
 
+    // private static final DB db;
+
     public static void main(String[] args) {
         final ObjectMapper mapper = new ObjectMapper();
+        final ObjectWriter prettyMapper = new ObjectMapper().writerWithDefaultPrettyPrinter();
 
         final User user = new User();
         user.name = "Bob";
@@ -17,6 +23,7 @@ public class Tester {
         try {
             final String result = mapper.writeValueAsString(user);
             System.out.println(result);
+            System.out.println(prettyMapper.writeValueAsString(user));
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
