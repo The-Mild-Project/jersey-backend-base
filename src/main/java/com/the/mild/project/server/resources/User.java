9fc99d0d1773570c720f7b8d2dec9c74ff369857
Alex Thompson <awt1088@gmail.com>
2020-03-20 16:16:24 -0700

    renamed create user to user, all user funcs can be added here

--- src/main/java/com/the/mild/project/server/resources/User.java
+++ src/main/java/com/the/mild/project/server/resources/User.java
@@ -2,19 +2,21 @@ package com.the.mild.project.server.resources;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoIterable;
+import com.mongodb.client.result.DeleteResult;
 import com.the.mild.project.MongoDatabaseType;
+import com.the.mild.project.db.mongo.DocumentEntry;
 import com.the.mild.project.db.mongo.MongoDatabaseFactory;
 import com.the.mild.project.db.mongo.MongoDocumentHandler;
 import com.the.mild.project.db.mongo.documents.UserDocument;
 import com.the.mild.project.db.mongo.exceptions.CollectionNotFoundException;
 import com.the.mild.project.db.mongo.exceptions.DocumentSerializationException;
 import com.the.mild.project.server.jackson.JacksonHandler;
-import com.the.mild.project.server.jackson.JacksonTest;
 import com.the.mild.project.server.jackson.UserJson;
+import org.bson.Document;
 
 import static com.the.mild.project.ResourceConfig.PATH_USER_RESOURCE;
 import static com.the.mild.project.ResourceConfig.PATH_CREATE;
+import static com.the.mild.project.ResourceConfig.PATH_LOGIN;
 import static com.the.mild.project.server.Main.MONGO_DB_FACTORY;
 
 import javax.inject.Singleton;
@@ -26,7 +28,7 @@ import javax.ws.rs.core.Response;
 
 @Singleton
 @Path(PATH_USER_RESOURCE)
-public class CreateUser {
+public class User {
 
     private static final MongoDatabaseFactory mongoFactory;
     private static final MongoDocumentHandler mongoHandlerDevelopTest;
@@ -51,6 +53,39 @@ public class CreateUser {
             mongoHandlerDevelopTest.tryInsert(document);
         } catch(JsonProcessingException | DocumentSerializationException | CollectionNotFoundException e) {
             e.printStackTrace();
+            return Response.status(404).build();
+        }
+        return Response.status(200).build();
+    }
+
+    @POST
+    @Path(PATH_LOGIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response loginUser(String userInfo) {
+        try {
+            UserJson user = JacksonHandler.unmarshal(userInfo, UserJson.class);
+            final UserDocument document = new UserDocument(user);
+
+            mongoHandlerDevelopTest.tryInsert("session", document);
+        } catch (JsonProcessingException | CollectionNotFoundException | DocumentSerializationException e) {
+            e.printStackTrace();
+            return Response.status(404).build();
+        }
+        return Response.status(200).build();
+    }
+
+    @POST
+    @Path(PATH_LOGIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response logoutUser(String userInfo) {
+        try {
+            UserJson user = JacksonHandler.unmarshal(userInfo, UserJson.class);
+            DocumentEntry<String> documentEntry = new DocumentEntry<>("googleId", user.getGoogleId());
+            Document document = mongoHandlerDevelopTest.tryFindOne("session", documentEntry);
+            DeleteResult result = mongoHandlerDevelopTest.TryDelete("session", document);
+        } catch (JsonProcessingException | CollectionNotFoundException e) {
+            e.printStackTrace();
+            return Response.status(404).build();
         }
         return Response.status(200).build();
     }
