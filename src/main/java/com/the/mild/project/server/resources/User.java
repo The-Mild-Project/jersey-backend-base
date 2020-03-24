4231eb72843b134db7de2608734eaaf28eded920
Alex Thompson <awt1088@gmail.com>
2020-03-24 12:59:23 -0700

    removed some commented out code

--- src/main/java/com/the/mild/project/server/resources/User.java
+++ src/main/java/com/the/mild/project/server/resources/User.java
@@ -1,32 +1,22 @@
 package com.the.mild.project.server.resources;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.mongodb.client.MongoDatabase;
 import com.the.mild.project.MongoDatabaseType;
-import com.the.mild.project.db.mongo.DocumentEntry;
 import com.the.mild.project.db.mongo.MongoDatabaseFactory;
 import com.the.mild.project.db.mongo.MongoDocumentHandler;
-import com.the.mild.project.db.mongo.documents.SessionDocument;
-import com.the.mild.project.db.mongo.documents.UserDocument;
 import com.the.mild.project.db.mongo.exceptions.CollectionNotFoundException;
 import com.the.mild.project.db.mongo.exceptions.DocumentSerializationException;
-import com.the.mild.project.server.jackson.JacksonHandler;
-import com.the.mild.project.server.jackson.SessionJson;
-import com.the.mild.project.server.jackson.UserJson;
 import org.bson.Document;
 
 import static com.the.mild.project.ResourceConfig.*;
 import static com.the.mild.project.server.Main.MONGO_DB_FACTORY;
 
 import javax.inject.Singleton;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -49,7 +39,6 @@ public class User {
 
     @GET
     @Path(PATH_CREATE)
-//    @Consumes(MediaType.APPLICATION_JSON)
     public Response createUser(@Context HttpHeaders header) {
         String googleId = header.getHeaderString(GOOGLE_ID);
 
@@ -66,21 +55,11 @@ public class User {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-//        try {
-//            UserJson user = JacksonHandler.unmarshal(userInfo, UserJson.class);
-//            final UserDocument document = new UserDocument(user);
-//
-//            mongoHandlerDevelopTest.tryInsert(document);
-//        } catch(JsonProcessingException | DocumentSerializationException | CollectionNotFoundException e) {
-//            e.printStackTrace();
-//            return Response.status(404).build();
-//        }
-        return Response.status(200).build();
+        return Response.status(Response.Status.ACCEPTED).build();
     }
 
     @GET
     @Path(PATH_LOGIN)
-//    @Consumes(MediaType.APPLICATION_JSON)
     public Response loginUser(@Context HttpHeaders header) {
         String googleId = header.getHeaderString(GOOGLE_ID);
 
@@ -97,7 +76,7 @@ public class User {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        return Response.status(200).build();
+        return Response.status(Response.Status.ACCEPTED).build();
     }
 
     @GET
@@ -117,9 +96,9 @@ public class User {
             Document result = mongoHandlerDevelopTest.tryDelete(SESSION_COLLECTION, document);
         } catch (CollectionNotFoundException | DocumentSerializationException e) {
             e.printStackTrace();
-            return Response.status(404).build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        return Response.status(200).build();
+        return Response.status(Response.Status.ACCEPTED).build();
     }
 }
